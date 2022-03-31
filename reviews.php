8387d20265c9d45d847bd9ff647c82ff570932fb
Anthony-BouKhalil <51209624+Anthony-BouKhalil@users.noreply.github.com>
2022-03-30 21:40:38 -0400

    Added review form which inputs reviews to the DB

--- reviews.php
+++ reviews.php
@@ -13,24 +13,100 @@ if (!isset($_SESSION)) {
     <link rel="stylesheet" type="text/css" href="styles/main-page.css">
     <link rel="stylesheet"  type="text/css" href="bootstrap/css/bootstrap.css"></link>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php 
-    //TODO:
-    //Refactor code to MVC Design Pattern & AngularJS Framework if needed
+  //TODO:
+  //Refactor code to MVC Design Pattern & AngularJS Framework if needed
 
-    require 'header.php';
-    $conn = mysqli_connect('localhost', 'root', '', 'scsstore'); 
+  require 'header.php';
+  $conn = mysqli_connect('localhost', 'root', '', 'scsstore'); 
 
-    //TODO:
-    //Implement form to input reviews
-    //Only if logged in 
-    
+  $errReview = "";
+  if(isset($_POST['submit'])) {
+    // Check if review has been entered
+    if(empty($_POST['review'])) {
+      $errReview = 'Please enter a review';
+    }
+    else {
+      $product_id = $_POST['product'];
+      $rating = $_POST['rating'];
+      $review = $_POST['review'];
+      $insert_query = "INSERT INTO review (product_id, text, stars) VALUES ('$product_id', '$review', '$rating')";                        
+      $insert = mysqli_query($conn, $insert_query);    
+    }
+  }
+
+?>
+  <div class="container">
+    <h2>Reviews</h2>
+    <p>Please complete the information below to review a product:</p>
+    <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <div class="form-group row">
+      <label for="inputProduct" class="col-sm-3 col-form-label">Select a Product <font color='red'>*</font></label>
+        <div class="col-sm-3">
+          <?php 
+            echo '<select id="inputProduct" class="form-control" name="product">;
+                    <option value="1">iPhone 11</option>;
+                    <option value="2">AirPods Pro</option>;
+                    <option value="3">Samsung Galaxy S22</option>;
+                    <option value="4">Beats Studio3 Wireless Headphones</option>;
+                    <option value="5">MacBook Air</option>;
+                    <option value="6">iPad mini</option>;
+                    <option value="7">Nikon Z6 II</option>;
+                    <option value="8">Google Pixel 6</option>;
+                    <option value="9">Dell XPS 15</option>;                    
+                    <option value="10">Amazon Kindle Paperwhite</option>;                    
+                    <option value="11">Microsoft Surface Pro 7</option>;                    
+                    <option value="12">iPad Pro</option>;
+                </select>'
+          ?>
+        </div>
+      </div>
+      <div class="form-group row">
+      <label for="inputRating" class="col-sm-3 col-form-label">Select a Rating <font color='red'>*</font></label>
+        <div class="col-sm-3">
+          <?php 
+            echo '<select id="inputRating" class="form-control" name="rating">;
+                    <option value="1">1</option>;
+                    <option value="2">2</option>;
+                    <option value="3">3</option>;
+                    <option value="4">4</option>;
+                    <option value="5">5</option>;
+                </select>'
+          ?>
+        </div>
+      </div>
+      <div class="form-group row">
+      <label for="inputReview" class="col-sm-3 col-form-label">Enter a Review <font color='red'>*</font></label>
+        <div class="col-sm-12">
+          <?php 
+            echo '<textarea id="inputReview" class="form-control" name="review" rows="10"></textarea>';
+            echo "<font color='red'>".$errReview."</font>";
+          ?>
+        </div>
+      </div>
+      <?php
+        if (isset($_SESSION['user'])) {
+            echo "<div class='form-group row'>
+                  <div class='offset-sm-3 col-sm-6'>
+                  <input type='submit' value='Submit' name='submit' class='btn btn-primary'/>
+                  </div>
+                  </div>";
+          }
+          else {
+                echo "<font color='red'>You must be signed in to checkout</font>";
+          }
+      ?>
+    </form>
+</div>
+      
+<?php 
     for ($i = 1; $i <= 12; $i++) {
-        $total_query = "SELECT COUNT(review.product_id) AS TotalReviews, ROUND(AVG(stars), 1) AS AverageRating, text, product.name
+        $total_query = "SELECT COUNT(review.product_id) AS TotalReviews, ROUND(AVG(stars), 1) AS AverageRating, text, product.name, product.image
                         FROM review
                         JOIN product ON product.product_id = review.product_id
                         WHERE review.product_id = $i";                        
@@ -40,12 +116,16 @@ if (!isset($_SESSION)) {
                           WHERE review.product_id = $i
                           ORDER BY stars DESC";                        
         $reviews = mysqli_query($conn, $reviews_query);
-
-        echo "<div class='col'>";
+        
+        echo "<div class='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>";
         echo "<div class='card shadow-sm'>";
-            echo "<div class='card-body'>";
-            echo "<div class='card-title'>";
             while($row = mysqli_fetch_assoc($total)) {
+                $name = $row["name"];
+                $address = $row["image"];
+                echo "<img width='100%' height='100%' alt='" . $name . "' src='" . $address . "'/>";
+                echo "</div>";
+                echo "<div class='card-body'>";
+                echo "<div class='card-title'>";
                 echo "<b>" . $row['name'] . "</b>";
                 echo "</div>";
                 echo "<p class='card-text'>Total Reviews (". $row['TotalReviews'] . ")</h5>";
