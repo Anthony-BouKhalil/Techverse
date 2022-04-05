01ae0c2ed5f6f9332d74d6c127c65e937cfebda1
Anthony-BouKhalil <51209624+Anthony-BouKhalil@users.noreply.github.com>
2022-04-05 11:33:21 -0400

    Encoded to json

--- reviews.php
+++ reviews.php
@@ -105,18 +105,32 @@ if (!isset($_SESSION)) {
 </div>
       
 <?php 
+    $reviews_by_product = array();
     for ($i = 1; $i <= 12; $i++) {
-        $total_query = "SELECT COUNT(review.product_id) AS TotalReviews, ROUND(AVG(stars), 1) AS AverageRating, text, product.name, product.image
+        $total_query = "SELECT COUNT(review.product_id) AS TotalReviews, ROUND(AVG(stars), 1) AS AverageRating, product.name, product.image
                         FROM review
                         JOIN product ON product.product_id = review.product_id
                         WHERE review.product_id = $i";                        
         $total = mysqli_query($conn, $total_query);
+
+
         $reviews_query = "SELECT text, stars
                           FROM review 
                           WHERE review.product_id = $i
                           ORDER BY stars DESC";                        
         $reviews = mysqli_query($conn, $reviews_query);
+
+    
+        $output = mysqli_fetch_assoc($total);
         
+        $reviews_text = array();
+        while($row = mysqli_fetch_assoc($reviews)) {
+            array_push($reviews_text, $row);
+        }
+
+        array_push($reviews_by_product, array($output, $reviews_text));
+        
+        /*
         echo "<div class='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>";
         echo "<div class='card shadow-sm'>";
             while($row = mysqli_fetch_assoc($total)) {
@@ -142,7 +156,10 @@ if (!isset($_SESSION)) {
             echo "</div>";
         echo "</div>";
         echo "</div>";
+    */
     } 
+    $data = mb_convert_encoding($reviews_by_product,'UTF-8', 'UTF-8');
+    echo json_encode($data);
 ?>
 <?php require 'footer.php' ?>
 </body>
